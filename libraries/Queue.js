2f2a0116d14f06a7a36cfc19ec889dad50361f84
ruiming <ruiming.zhuang@gmail.com>
2017-03-08 13:15:04 +0800

    添加缓存和消息队列

---
+++ libraries/Queue.js
@@ -0,0 +1,23 @@
+const Redis = require('ioredis')
+const queueConfig = require('config').get('Queue')
+
+const Q = new Redis(queueConfig.port, queueConfig.host, {
+  password: queueConfig.auth,
+  db: queueConfig.db
+})
+
+Q.on('connect', () => console.log('消息队列连接成功'))
+Q.on('disconnect', () => console.log('消息队列连接失败'))
+Q.on('error', () => console.log('消息队列连接出错'))
+
+const queue = {
+  createJob: async function (target, data) {
+    await Q.lpush(target, JSON.stringify(data))
+  },
+  submitTask: async function (submitId) {
+    await queue.createJob('JUDGER', {
+      event: 'JUDGE_SUBMISSION',
+      payload: submitId
+    })
+  }
+}
