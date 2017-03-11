70e69dc9d0ee9ddca887d02866ef5311ff86f14e
ruiming <ruiming.zhuang@gmail.com>
2017-03-11 20:53:26 +0800

    添加用户登录接口

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,19 @@ const Router = require('koa-router')
 
 const router = new Router()
 
-router.post('/', async (ctx, next) => {
+router.post('/register', async (ctx, next) => {
   await UserService.register(ctx.request.body.name, ctx.request.body.email, ctx.request.body.password)
   ctx.body = {
     success: true,
     data: '注册成功'
+  }
+})
+
+router.post('/login', async (ctx, next) => {
+  await UserService.login(ctx.request.body.name || ctx.request.body.email, ctx.request.body.password)
+  ctx.body = {
+    succes: true,
+    data: '登录成功'
   }
 })
 
