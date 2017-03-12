8839ea7cd91b8e803f313892cee1ac51d0921b29
ruiming <ruiming.zhuang@gmail.com>
2017-03-12 21:26:31 +0800

    修复测试

--- tests/services/problem.js
+++ tests/services/problem.js
@@ -21,22 +21,22 @@ describe('Problem', function () {
   })
 
   it('call getProblemById with an exist id should be ok', async () => {
-    const problem = await ProblemService.getProblemById({ id: problems[0].id })
+    const problem = await ProblemService.getProblemById(problems[0].id)
     problem.id.should.equal(problems[0].id)
   })
 
   it('call getProblemById with nothing should throw error', async () => {
-    let error = await ProblemService.getProblemById({}).catch(e => e)
+    let error = await ProblemService.getProblemById().catch(e => e)
     error.should.be.an.instanceof(ParamsError)
   })
 
   it('call getProblemById with an unexisted id should throw error', async () => {
-    let error = await ProblemService.getProblemById({id: 1251521525}).catch(e => e)
+    let error = await ProblemService.getProblemById(1251521525).catch(e => e)
     error.should.be.an.instanceof(ParamsError)
   })
 
   it('call getProblemById with an error type of id should throw error', async () => {
-    let error = await ProblemService.getProblemById({id: 'c314fag'}).catch(e => e)
+    let error = await ProblemService.getProblemById('c314fag').catch(e => e)
     error.should.be.an.instanceof(ParamsError)
   })
 })
