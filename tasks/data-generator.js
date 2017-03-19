49939d7d25f2e932404156e14b7db17b3e9b0865
ruiming <ruiming.zhuang@gmail.com>
2017-03-19 13:41:49 +0800

    移除setCookie方法

--- tasks/data-generator.js
+++ tasks/data-generator.js
@@ -5,6 +5,7 @@ const ProblemModel = require('../models/problem')
 const SubmissionModel = require('../models/submission')
 const UserModel = require('../models/user')
 const Queue = require('../libraries/Queue')
+const { SHA256 } = require('crypto-js')
 
 const inputData = '1 2'
 const outputData = '3'
@@ -19,13 +20,19 @@ int main() {
 
 before(async function () {
   // 创建两个用户
-  const users = await UserModel[Symbol.for('create')]({}, {})
+  const users = await UserModel[Symbol.for('create')]({
+    name: 'test',
+    password: SHA256('testtest').toString()
+  }, {})
+  let problems = []
   // 每个用户创建 6 条 Problem 表的 Mock 数据
-  const problems = await Promise.all(users.map(user => ProblemModel[Symbol.for('create')](
-    { userId: user.id, inputData, outputData }, { userId: user.id, inputData, outputData },
-    { userId: user.id, inputData, outputData }, { userId: user.id, inputData, outputData },
-    { userId: user.id, inputData, outputData }, { userId: user.id, inputData, outputData }
-  )))
+  for (let i = 0; i < 10; i++) {
+    problems = await Promise.all(users.map(user => ProblemModel[Symbol.for('create')](
+      { userId: user.id, inputData, outputData }, { userId: user.id, inputData, outputData },
+      { userId: user.id, inputData, outputData }, { userId: user.id, inputData, outputData },
+      { userId: user.id, inputData, outputData }, { userId: user.id, inputData, outputData }
+    )))
+  }
   // 创建一个正确的 submission
   const [submission] = await SubmissionModel[Symbol.for('create')]({
     problemId: problems[0][0].id,
