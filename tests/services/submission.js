396eeabab7be5387cff0a5b04c1047a48fe9d699
ruiming <ruiming.zhuang@gmail.com>
2017-03-26 21:41:59 +0800

    完善测试覆盖

--- tests/services/submission.js
+++ tests/services/submission.js
@@ -1,25 +1,10 @@
-require('should')
 const SubmissionService = require('../../services/submission')
-const ProblemModel = require('../../models/problem')
-const SubmissionModel = require('../../models/submission')
 
-let problems = []
+const { ParamsError } = require('../../libraries/Error')
 
 describe('Service: Submission', function () {
-  before(async function () {
-    problems = await ProblemModel[Symbol.for('create')]({ userId: USER.id }, { userId: USER.id }, { userId: USER.id }, { userId: USER.id }, { userId: USER.id })
-  })
-
-  after(async function () {
-    await Promise.all(problems.map(problem => problem.destroy()))
-  })
-
-  it('Func: checkSubmission', async () => {
-    const submissionId = await SubmissionService.checkSubmission(USER.id, problems[0].id, '12345', 'cc')
-    const submission = await SubmissionModel.findById(submissionId)
-    submission.problemId.should.equal(problems[0].id)
-    submission.code.should.equal('12345')
-    submission.lang.should.equal('cc')
-    await submission.destroy()
+  it('Call checkSubmission with an invalid problemId', async () => {
+    const error = await SubmissionService.checkSubmission(USER.id, '123123', '12345', 'cc').catch(e => e)
+    error.should.be.an.instanceOf(ParamsError)
   })
 })
