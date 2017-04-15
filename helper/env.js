1fde05cbd3c10f172e5669b62e02ab72f53dde7c
ruiming <ruiming.zhuang@gmail.com>
2017-04-15 17:08:46 +0800

    fix(helper/env): Proxy 改为拷贝修改对象

--- helper/env.js
+++ helper/env.js
@@ -1,8 +1,18 @@
-module.exports = config => new Proxy(config, {
-  get (target, key) {
-    if (typeof target[key] === 'string' && target[key].toUpperCase() === target[key]) {
-      return process.env[target[key]] || target[key]
+// module.exports = config => new Proxy(config, {
+//   get (target, key) {
+//     if (typeof target[key] === 'string' && target[key].toUpperCase() === target[key]) {
+//       return process.env[target[key]] || target[key]
+//     }
+//     return target[key]
+//   }
+// })
+
+module.exports = config => {
+  const items = JSON.parse(JSON.stringify(config))
+  for (const key in items) {
+    if (typeof items[key] === 'string' && items[key].toUpperCase() === items[key]) {
+      items[key] = process.env[items[key]] || items[key]
     }
-    return target[key]
   }
-})
+  return items
+}
