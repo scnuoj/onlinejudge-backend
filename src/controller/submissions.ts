0f291b26675abb2b1c7d452f71e5641bb7f60e35
ruiming <ruiming.zhuang@gmail.com>
2017-06-07 20:29:09 +0800

    feat: add authorization to submission route

--- src/controller/submissions.ts
+++ src/controller/submissions.ts
@@ -17,6 +17,7 @@ export class PostSubmissionData {
 @Controller('/v1/submissions')
 export class SubmissionsController {
   @Post('/')
+  @UseBefore(authorization())
   async index (@Ctx() ctx: Context, @Body() submission: PostSubmissionData) {
     const submissionId = await ctx.services.submissions.create(ctx.state.user.id, submission.id, submission.code, submission.lang)
     ctx.ok(submissionId)
