02751e73d6db31e00cfff7433db299c1611c483e
ruiming <ruiming.zhuang@gmail.com>
2017-06-20 11:56:29 +0800

    feat: update problemRepository

--- src/repository/ProblemRepository.ts
+++ src/repository/ProblemRepository.ts
@@ -1,6 +1,9 @@
 import { EntityRepository, Repository } from 'typeorm'
 import { Problem } from 'app/entity/Problem'
 import { Service } from 'typedi'
+import { BadRequestError } from 'routing-controllers'
+
+export type IOrder = 'ASC' | 'DESC'
 
 @Service()
 @EntityRepository(Problem)
@@ -14,6 +17,28 @@ export class ProblemRepository extends Repository<Problem> {
           user: 'problem.user'
         }
       }
+    })
+  }
+
+  public getProblems(offset: number, limit: number, sortby: string, order: IOrder) {
+    return this.findAndCount({
+      skip: offset,
+      take: limit,
+      order: {
+        [sortby]: order
+      },
+      join: {
+        alias: 'problem',
+        innerJoinAndSelect: {
+          user: 'problem.user'
+        }
+      }
+    })
+  }
+
+  public getRecommemdProblems (id: number) {
+    return this.find({
+      take: 5
     })
   }
 
--- src/service/ProblemService.ts
+++ src/service/ProblemService.ts
@@ -1,12 +1,10 @@
 import { BadRequestError } from 'routing-controllers'
-import { Problem } from 'app/model/Problem'
-import { User } from 'app/model/User'
 import { Service } from 'typedi'
 import { OrmRepository, OrmCustomRepository } from 'typeorm-typedi-extensions'
-import { Problem as TProblem } from 'app/entity/Problem'
-import { User as TUser } from 'app/entity/User'
-import { Repository } from 'typeorm'
+import { Problem } from 'app/entity/Problem'
 import { ProblemRepository } from 'app/repository/ProblemRepository'
+
+export type IOrder = 'ASC' | 'DESC'
 
 @Service()
 export class ProblemService {
@@ -14,42 +12,19 @@ export class ProblemService {
   @OrmCustomRepository(ProblemRepository)
   private problemRepository: ProblemRepository
 
-  @OrmRepository(TUser)
-  private userRepository: Repository<TUser>
-
-  public async show (id: number): Promise<Problem> {
-    const problem = await Problem.findById<Problem>(id, {
-      include: [{
-        model: User,
-        as: 'user',
-        attributes: ['name', 'id', 'avatar', 'gender', 'school', 'email', 'remark']
-      }]
-    })
-    const row = await this.problemRepository.getProblem(id)
-    console.log(row)
-    if (problem) {
-      return problem
-    } else {
-      throw new BadRequestError('题号有误')
+  public async show (id: number) {
+    const problem = await this.problemRepository.getProblem(id)
+    if (!problem) {
+      throw new BadRequestError(`题号有误: ${id}`)
     }
+    return problem
   }
 
-  public async list (offset: number, limit: number, sortby: string, order: string): Promise<{ rows: Problem[]; count: number; } > {
-    return await Problem.findAndCountAll<Problem>({
-      limit,
-      offset,
-      order: [[sortby, order]],
-      include: [{
-        model: User,
-        as: 'user',
-        attributes: ['name', 'id', 'avatar', 'gender', 'school', 'email', 'remark']
-      }]
-    })
+  public async list (offset: number, limit: number, sortby: string, order: IOrder) {
+    return this.problemRepository.getProblems(offset, limit, sortby, order)
   }
 
-  public async recommend (id: number): Promise<Problem[] > {
-    return await Problem.findAll<Problem>({
-      limit: 5
-    })
+  public async recommend (id: number) {
+    return this.problemRepository.getRecommemdProblems(id)
   }
 }
